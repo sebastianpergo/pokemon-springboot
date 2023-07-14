a4c20b21455572e912692628bb563158e2e2d4e0
sebastianpergo <sebastianperdomopg@gmail.com>
2023-07-14 12:29:30 -0500

    feat(PokemonRepo): Created

--- src/main/java/com/demo/pokemon/model/PokemonModel.java
+++ src/main/java/com/demo/pokemon/model/PokemonModel.java
@@ -1,11 +1,29 @@
 package com.demo.pokemon.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
-@Table
+@NoArgsConstructor
+@AllArgsConstructor
+// @Data same as @Getter @Setter @RequiredArgsConstructor @ToString @EqualsAndHashCode
+@Getter
+@Setter
+@Table(name = "tb_pokemon")
 public class PokemonModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idPokemon;
 
+    @Column(nullable = false)
+    private String name;
 
+    @Column(nullable = false)
+    private Long health;
+
+    @Column(nullable = false)
+    private Long attack;
+
+    @Column(nullable = false)
+    private char status;
 }
---
+++ src/main/java/com/demo/pokemon/repository/PokemonRepository.java
@@ -0,0 +1,4 @@
+package com.demo.pokemon.repository;
+
+public interface PokemonRepository {
+}
